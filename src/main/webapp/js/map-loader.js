26ec5c028e287cfc4d30ef455d86a01833b53913
Shanlan <melodyli710@gmail.com>
2019-06-10 06:55:56 +0800

    fix semicolon

--- src/main/webapp/js/map-loader.js
+++ src/main/webapp/js/map-loader.js
@@ -2,11 +2,11 @@
 function createMap(){
   const map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 1.34, lng: 103.8},
-    zoom: 12
+    zoom: 11
   });
   addLandmark(map, 1.280547, 103.844502, 'Maxwell Food Centre', '1 Kadayanallur St 069184');
   addLandmark(map, 1.308398, 103.885808, 'Old Airport Road Food Centre', '51 Old Airport Rd, Singapore 390051');
-  addLandmark(map, 1.331814, 103.938867, 'Bedok 85 Market', '85 Bedok North Street 4, Singapore 460085')
+  addLandmark(map, 1.331814, 103.938867, 'Bedok 85 Market', '85 Bedok North Street 4, Singapore 460085');
 }
 
 /* Add landmark to map */
