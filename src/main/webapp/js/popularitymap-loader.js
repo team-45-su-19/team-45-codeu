85db83c535d22566c92dd29f4e63b6b1e3a4e9db
Jiang Yuxin <jyx11011@gmail.com>
2019-06-13 19:07:17 +0800

    modify the map page to show popularity

---
+++ src/main/webapp/js/popularitymap-loader.js
@@ -0,0 +1,54 @@
+function createMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 1.34, lng: 103.8},
+        zoom: 12
+    });
+    map.addListener('click', (event) => {
+        createMarker(event.latLng.lat(), event.latLng.lng());
+        postMarker(event.latLng.lat(), event.latLng.lng());
+    });
+    fetchMarkers();
+    var heatmapbutton = document.getElementById("heatmap");
+    heatmapbutton.onclick = showHeatMap;
+}
+
+function fetchMarkers(){
+    pos = [];
+    fetch('/markers').then((response) => {
+        return response.json();
+    }).then((markers) => {
+        markers.forEach((marker) => {
+            pos.push([marker.lat, marker.lng]);
+            createMarker(marker.lat, marker.lng);
+        });
+    });
+}
+
+function createMarker(lat, lng){
+    const marker = new google.maps.Marker({
+        position: {lat: lat, lng: lng},
+        map: map
+    });
+}
+
+function postMarker(lat, lng){
+    pos.push([lat,lng]);
+    const params = new URLSearchParams();
+    params.append('lat', lat);
+    params.append('lng', lng);
+    fetch('/markers', {
+        method: 'POST',
+        body: params
+    });
+}
+
+function showHeatMap() {
+    var heatmapData = [];
+    pos.forEach((marker)=>{
+        heatmapData.push(new google.maps.LatLng(marker[0], marker[1]));
+    })
+    var heatmap = new google.maps.visualization.HeatmapLayer({
+        data: heatmapData
+    });
+    heatmap.setMap(map);
+}
