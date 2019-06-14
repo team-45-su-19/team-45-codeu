5de7ec54000cdab3e3449677c0c7b0b69e4c3d1f
Caleb Mah <calebmah@gmail.com>
2019-06-14 23:17:27 +0800

    Reformat stats page with bootstrap

--- src/main/webapp/js/stats-loader.js
+++ src/main/webapp/js/stats-loader.js
@@ -1,30 +1,4 @@
-// Fetch stats and display them in the page.
-function fetchStats(){
-  const url = '/stats';
-  fetch(url).then((response) => {
-    return response.json();
-  }).then((stats) => {
-    const statsContainer = document.getElementById('stats-container');
-    statsContainer.innerHTML = '';
-
-    const messageCountElement = buildStatElement('Total message count: ' + stats.messageCount);
-    statsContainer.appendChild(messageCountElement);
-
-    const userCountElement = buildStatElement('Total user count: ' + stats.userCount);
-    statsContainer.appendChild(userCountElement);
-
-    const averageMessageLengthElement = buildStatElement('Average message length: ' + stats.averageMessageLength);
-    statsContainer.appendChild(averageMessageLengthElement);
-  });
-}
-
-// Build html p element from string
-function buildStatElement(statString){
-  const statElement = document.createElement('p');
-  statElement.appendChild(document.createTextNode(statString));
-  return statElement;
-}
-
+// Draws stats charts
 function buildChart(stats){
   google.charts.load('current', {packages: ['corechart']});
   google.charts.setOnLoadCallback(function() {drawChart(stats)});
@@ -48,11 +22,11 @@ function buildChart(stats){
         ["Average message length", stats.averageMessageLength, stats.averageMessageLength],
       ]);
 
-      let chart = new google.visualization.BarChart(document.getElementById('stats-chart'));
+      let chart = new google.visualization.ColumnChart(document.getElementById('stats-chart'));
       let chart_options = {
-        width: 600,
-        height: 300,
-        chartArea: {left:'25%'},
+        height: document.getElementById('stats-chart').getBoundingClientRect().width/2,
+        width: document.getElementById('stats-chart').getBoundingClientRect().width,
+        backgroundColor: 'transparent',
         legend: {position:'none'}
       };
       chart.draw(stats_data, chart_options);
@@ -62,6 +36,5 @@ function buildChart(stats){
 
 // Fetch data and populate the UI of the page.
 function buildUI(){
-  fetchStats();
   buildChart();
 }
