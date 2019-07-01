cb564745cfd761951cff0b809b6b532013500bf4
Caleb Mah <calebmah@gmail.com>
2019-07-02 02:39:44 +0800

    Change list to list group

--- src/main/webapp/js/community-loader.js
+++ src/main/webapp/js/community-loader.js
@@ -8,25 +8,26 @@ function fetchUserList(){
     list.innerHTML = '';
 
     users.forEach((user) => {
-     const userListItem = buildUserListItem(user);
-     list.appendChild(userListItem);
+      const userListItem = buildUserListItem(user);
+      list.appendChild(userListItem);
     });
   });
 }
 
 /**
- * Builds a list element that contains a link to a user page, e.g.
- * <li><a href="/user-page.html?user=test@example.com">test@example.com</a></li>
+ * Builds a link element that contains a link to a user page, e.g.
+ * <a href="/user-page.html?user=test@example.com" class="list-group-item list-group-item-action justify-content-between align-items-center">test@example.com</a>
  */
 function buildUserListItem(user){
   const userLink = document.createElement('a');
   userLink.setAttribute('href', '/user-page.html?user=' + user);
   userLink.appendChild(document.createTextNode(user));
-  const userListItem = document.createElement('li');
-  userListItem.classList.add('list-unstyled-item');
-  userListItem.classList.add('list-hours-item');
-  userListItem.appendChild(userLink);
-  return userListItem;
+  userLink.classList.add('list-group-item');
+  userLink.classList.add('list-group-item-action');
+  userLink.classList.add('justify-content-between');
+  userLink.classList.add('align-items-center');
+
+  return userLink;
 }
 
 /** Fetches data and populates the UI of the page. */
